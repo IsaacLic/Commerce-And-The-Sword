1808a66670fc8c11ff5ad1a590062d6a3391a1c3
Isaac Lichter <isaacilichter@gmail.com>
2019-04-14 11:15:46 -0400

    added HashMaps

--- src/commerceandthesword/Game.java
+++ src/commerceandthesword/Game.java
@@ -1,6 +1,11 @@
 
 package commerceandthesword;
 
+import java.util.HashMap;
+
 public class Game {
+    
+    private HashMap<String, Tile> tileList;
+    private HashMap<String, Territory> territoryList;
     
 }
